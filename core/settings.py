347d231a9bdc2a991e4ca7fe8b015077a2a3d570
Mukesh Kumar Ray <mukeshray995531@gmail.com>
2026-02-01 15:46:28 +0530

    Prepare project for Render deployment

--- core/settings.py
+++ core/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0jf)irg$5zvv4kmhh&f8pr&^vinz2@1ns=161tr*360ikfw411'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,3 +122,4 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_ROOT = BASE_DIR / "downloads"
 MEDIA_URL = "/media/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
